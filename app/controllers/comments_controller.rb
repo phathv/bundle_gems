3e8fb34616c5265b1fad0a6562257b2167e0e06e
Kevin <kevin@vinova.sg>
2011-08-19 16:40:46 +0700

    check authen for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < InheritedResources::Base
   belongs_to :product #using this method when mapping in routes
   respond_to :js, :only => [:create, :delete]
+  before_filter :authenticate_user!, :except => [:index]
   #respond_to :html, :xml, :json
   def create
     create! { collection_url} #product_comments_path
